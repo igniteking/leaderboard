8b8f470e359fa9d631082b89a1621497b5072946
aarush005 <asta005@gmail.com>
2023-04-19 15:07:37 +0530

    .

--- spardha2023.php
+++ spardha2023.php
@@ -12,90 +12,6 @@
   </div>
 </div>
 
-<<<<<<< HEAD
-
-<div class="site-section" data-aos="fade-up">
-  <div class="container">
-    <div class="row align-items-center">
-      <div class="previous-about">
-        <div class="container">
-          <div class="intro">
-            <h1 class="text-center">SPARDHA 2023</h1>
-          </div>
-        </div>
-
-        <p class="text-center justify-content">NIMS University Rajasthan, Jaipur has announced SPARDHA 2023 with a bang. The students are back with new energy and improved stamina to fight on the field. <br>And this time, we have something special in store for the SPARDHA participants and audience – The Stalls from Different Colleges/Institutes </p>
-      </div>
-    </div>
-  </div>
-</div>
-
-
-<div class="site-section" data-aos="fade-up">
-  <div class="container">
-    <div class="row align-items-center">
-      <div class="previous-about">
-        <div class="container">
-
-          <h1 class="text-center">The Games We Host</h1>
-
-          <p>The university hosts 16 sports to ensure maximum participation in the event. NIMS University Rajasthan, Jaipur believes in giving equal opportunities to all and Spardha becomes the best way of doing it. </p>
-          <table class="table table-bordered" style="margin-top: 30px; margin-bottom:100px;">
-            <thead>
-              <tr>
-                <th scope="col">Basketball</th>
-                <th scope="col">Vollyball</th>
-              </tr>
-            </thead>
-            <thead>
-              <tr>
-                <th scope="col">Football</th>
-                <th scope="col">Cricket</th>
-              </tr>
-            </thead>
-            <thead>
-              <tr>
-                <th scope="col"> Kabaddi</th>
-                <th scope="col"> Kho-Kho</th>
-              </tr>
-            </thead>
-            <thead>
-              <tr>
-                <th scope="col"> Table Tennis</th>
-                <th scope="col">Badminton</th>
-              </tr>
-            </thead>
-            <thead>
-              <tr>
-                <th scope="col">Chess</th>
-                <th scope="col"> Carrom</th>
-              </tr>
-            </thead>
-            <thead>
-              <tr>
-                <th scope="col"> Tug of War</th>
-                <th scope="col"> Athietics</th>
-              </tr>
-            </thead>
-            <thead>
-              <tr>
-                <th scope="col"> Shot Put</th>
-                <th scope="col"> Discus Throw</th>
-              </tr>
-            </thead>
-            <thead>
-              <tr>
-                <th scope="col">Javelin Throw</th>
-                <th scope="col"> Long Jump</th>
-              </tr>
-            </thead>
-
-          </table>
-        </div>
-      </div>
-    </div>
-  </div>
-=======
 <div class="previous-about" style="margin-top: 100px">
 <div class="container">
   <div class="intro">
@@ -104,7 +20,6 @@
   </div>
   <p class="text-center justify-content">NIMS University Rajasthan, Jaipur has announced SPARDHA 2023 with a bang. The students are back with new energy and improved stamina to fight on the field.</p>
 </div>
->>>>>>> 2888939cba1ea379509e7aba15501f5dc5b112cb
 </div>
 
 <div class="site-section" data-aos="fade-up">
