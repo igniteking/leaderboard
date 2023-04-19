72fad8d4728a9572534ca7129e7cd4a87c77ef5f
aarush005 <asta005@gmail.com>
2023-04-19 17:44:19 +0530

    about sparda updated

--- video.php
+++ video.php
@@ -1,6 +1,33 @@
 <?php include('./components/header.php'); ?>
 
 
+<div class="site-blocks-cover overlay" style="background-image: url(./assets/images/banner-3.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+  <div class="container">
+    <div class="row align-items-center justify-content-start">
+      <div class="col-md-6 text-center text-md-left" data-aos="fade-up" data-aos-delay="400">
+        <h1 class="bg-text-line ">Spardha 2023</h1>
+        <p class="mt-4 text-justify">NIMS University Rajasthan, Jaipur has announced SPARDHA 2023 with a bang. The students are back with new energy and improved stamina to fight on the field.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
 
+
+<div class="site-section" data-aos="fade-up">
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="previous-about">
+        <div class="container">
+          <div class="intro">
+            <h1 class="text-center">SPARDHA 2023</h1>
+          </div>
+        </div>
+
+        <p class="text-center justify-content">NIMS University Rajasthan, Jaipur has announced SPARDHA 2023 with a bang. The students are back with new energy and improved stamina to fight on the field. <br>And this time, we have something special in store for the SPARDHA participants and audience – The Stalls from Different Colleges/Institutes </p>
+      </div>
+    </div>
+  </div>
+</div>
 
 <?php include('./components/footer.php') ?>
