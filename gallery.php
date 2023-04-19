3a676ac1b2cb3ccdf73fb5d78fc6323d741a08e4
aarush005 <asta005@gmail.com>
2023-04-19 14:40:26 +0530

    merge

--- gallery.php
+++ gallery.php
@@ -17,10 +17,34 @@
                 <p class="text-center">. </p>
             </div>
             <div class="row photos">
-                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/images/game.jpg" data-lightbox="photos"><img class="img-fluid" src="assets/images/game.jpg"></a></div>
-                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/images/game2.jpg" data-lightbox="photos"><img class="img-fluid" src="assets/images/game2.jpg"></a></div>
-                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/images/game3.jpg" data-lightbox="photos"><img class="img-fluid" src="assets/images/game3.jpg"></a></div>
-                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/images/game4.jpg" data-lightbox="photos"><img class="img-fluid" src="assets/images/game4.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/images/Gallery/001.jpg" data-lightbox="photos"><img class="img-fluid" src="assets/images/Gallery/001.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/images/Gallery/002.jpg" data-lightbox="photos"><img class="img-fluid" src="assets/images/Gallery/002.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/images/Gallery/003.jpg" data-lightbox="photos"><img class="img-fluid" src="assets/images/Gallery/003.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/images/Gallery/004.jpg" data-lightbox="photos"><img class="img-fluid" src="assets/images/Gallery/004.jpg"></a></div>
+            </div>
+            <div class="row photos">
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/images/Gallery/005.jpg" data-lightbox="photos"><img class="img-fluid" src="assets/images/Gallery/005.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/images/Gallery/006.jpg" data-lightbox="photos"><img class="img-fluid" src="assets/images/Gallery/006.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/images/Gallery/007.jpg data-lightbox="photos"><img class="img-fluid" src="assets/images/Gallery/007.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/images/Gallery/008.jpg" data-lightbox="photos"><img class="img-fluid" src="assets/images/Gallery/008.jpg"></a></div>
+            </div>
+            <div class="row photos">
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/images/Gallery/009.jpg" data-lightbox="photos"><img class="img-fluid" src="aassets/images/Gallery/009.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/images/Gallery/010.jpg" data-lightbox="photos"><img class="img-fluid" src="assets/images/Gallery/010.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/images/Gallery/011.jpg" data-lightbox="photos"><img class="img-fluid" src="assets/images/Gallery/011.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/images/Gallery/012.jpg" data-lightbox="photos"><img class="img-fluid" src="assets/images/Gallery/012.jpg"></a></div>
+            </div>
+            <div class="row photos">
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/images/Gallery/013.jpg" data-lightbox="photos"><img class="img-fluid" src="assets/images/Gallery/013.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/images/Gallery/014.jpg" data-lightbox="photos"><img class="img-fluid" src="assets/images/Gallery/014.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/images/Gallery/014.jpg" data-lightbox="photos"><img class="img-fluid" src="assets/images/Gallery/014.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/images/Gallery/015.jpg" data-lightbox="photos"><img class="img-fluid" src="assets/images/Gallery/015.jpg"></a></div>
+            </div>
+            <div class="row photos">
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/images/Gallery/016.jpg" data-lightbox="photos"><img class="img-fluid" src="assets/images/Gallery/016.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/images/Gallery/017.jpg" data-lightbox="photos"><img class="img-fluid" src="assets/images/Gallery/017.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/images/Gallery/018.jpg" data-lightbox="photos"><img class="img-fluid" src="assets/images/Gallery/018.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/images/Gallery/001.jpg" data-lightbox="photos"><img class="img-fluid" src="assets/images/Gallery/001.jpg"></a></div>
             </div>
         </div>
     </div>
